7367efa33f02ca5dbe98b98a4ee0ad1af2e3fa2d
mustafa <mustafaezzi143@gmail.com>
2025-05-04 07:45:36 +0500

    changed the indec page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   components: true,
@@ -44,6 +45,7 @@ export default defineNuxtConfig({
       anchorLinks: false,
     },
   },
+  
   // vueuse/motion
   runtimeConfig: {
     public: {
