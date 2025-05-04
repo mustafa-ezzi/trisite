b8b878cddb518df69d3d0c8424c583ac0bff601d
mustafa <mustafaezzi143@gmail.com>
2025-05-04 07:25:40 +0500

    changed the indec page

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,25 @@
 <script setup>
 import ThemeSwitcher from "~/components/ThemeSwitcher.vue";
 import SecondaryNav from "~/components/Navbar/SecondaryNav.vue";
-import Hero from "~/components/HomepageOne/Hero.vue";
-import Clients from "~/components/Shared/Clients.vue";
-import Services from "~/components/Shared/Services.vue";
-import Counter from "~/components/Shared/Counter.vue";
-import Vision from "~/components/HomepageOne/Vision.vue";
-import Solution from "~/components/HomepageOne/Solution.vue";
-import Integration from "~/components/HomepageOne/Integration.vue";
-import Faq from "~/components/HomepageOne/Faq.vue";
+import AiHero from "~/components/HomepageEight/AiHero.vue";
+import GetStart from "~/components/HomepageEight/GetStart.vue";
+import AiSteps from "~/components/HomepageEight/AiSteps.vue";
+import RobustFeature from "~/components/HomepageEight/RobustFeature.vue";
+import Pricing from "~/components/Shared/Pricing.vue";
+// import AiIntegration from "~/components/HomepageEight/AiIntegration.vue";
 import Testimonial from "~/components/Shared/Testimonial/Testimonial.vue";
-import Blog from "~/components/Shared/Blog.vue";
-import Cta from "~/components/Shared/Cta.vue";
-
-useHead({
-  title: "Trisite Solutions",
-});
+import Footer from "~/components/Footer.vue";
 </script>
 
 <template>
   <ThemeSwitcher />
   <SecondaryNav />
-  <Hero />
-  <Clients />
-  <Services />
-  <Counter />
-  <Vision />
-  <Solution />
-  <Integration />
-  <Faq />
+  <AiHero />
+  <GetStart />
+  <AiSteps />
+  <RobustFeature />
+  <Pricing />
+  <!-- <AiIntegration /> -->
   <Testimonial />
-  <Blog class="pb-150" />
-  <Cta title="Start Your Free Trial Today." />
   <Footer />
 </template>
