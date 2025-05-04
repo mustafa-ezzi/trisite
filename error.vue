e8c1f01a99789cf3b25631470ecbedc1ef8b4742
mustafa <mustafaezzi143@gmail.com>
2025-05-04 07:28:06 +0500

    changed the indec page

--- error.vue
+++ error.vue
@@ -1,16 +1,3 @@
-<script setup>
-import Footer from "~/components/Footer.vue";
-import CtaV3 from "~/components/HomepageThree/CtaV3.vue";
-import SecondaryNav from "~/components/Navbar/SecondaryNav.vue";
-import NotFound from "~/components/NotFound.vue";
-useHead({
-  title: "Not Found Page ",
-});
-</script>
-
 <template>
-  <SecondaryNav />
-  <NotFound />
-  <CtaV3 />
-  <Footer />
+  
 </template>
